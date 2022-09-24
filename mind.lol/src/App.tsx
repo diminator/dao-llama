4c36642c6709f8cca5503e136013fd7b37de98f3
diminator <dimi.dejonghe@gmail.com>
2022-09-25 00:31:02 +0100

    files

---
+++ mind.lol/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='llama.jpeg' className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;

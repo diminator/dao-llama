4fc0093ef2b7fed8d88d221c5261f6ce85d087e1
diminator <dimi.dejonghe@gmail.com>
2022-09-25 01:54:49 +0100

    all llamas live

--- mind.lol/src/App.tsx
+++ mind.lol/src/App.tsx
@@ -5,11 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='llama.jpeg' className="App-logo" alt="logo" />
+        <img src='https://studio-abraham.infura-ipfs.io/ipfs/QmX7GyFD36EXYtpsZNBuUK17U7YUjqP7KHwFJozXkeEgDQ?meta=QmVSpYk7Zan8KHmhKKKnbpKzPFoKVRUU26CfPUVDS2aeoP&thumb=QmWzW9Gt7k1djLtnoVsZSah9dXf6moHUqk66HSpLZtf8bV' className="App-logo" alt="logo" />
         <img src='https://studio-abraham.infura-ipfs.io/ipfs/QmaL7WXHto7zM1yZzjsXFfFr1QTf3r7scSM8XBbJzPcumx?meta=QmXqo66x9gFzLHmfnYVusxwZDkeKPkQXJNjk1KbRc3mLgE&thumb=QmWsyzHjXXFcnuHjkk5NbShbpqDtk4jeaka8dKih6ehqJR' className="App-logo" alt="logo" />
         <img src='https://studio-abraham.infura-ipfs.io/ipfs/QmT1cV1x8AVavERHNW4gM4XkHxf1ZMRjoSCjfXFbWtdWh7?meta=QmaT6CNeTNdNs7U8yuAMJ1WfXUQYurFdFTaRT6QsZktFhi&thumb=QmXoCoyRkME3ftNMJQJt58sWvm98ijZNtMkHDSNTCjA813' className="App-logo" alt="logo" />
         <img src='https://studio-abraham.infura-ipfs.io/ipfs/QmXBfw3BqBz1JxoEfq2Rp2f7C62DTddH9wM4j5HfHENfHQ?meta=QmXfPxFoRUjNgpNn2sR3Wicn6Hyi8q1FtAjs91BNHPE3g7&thumb=QmSe7f3LFKgQv7t1hjz1gxos1bYSCdW8XgJitGdaTRxN4f' className="App-logo" alt="logo" />
-
       </header>
     </div>
   );

abbf0e2e0826b0a05e14a385b8e3b235cba88b45
diminator <dimi.dejonghe@gmail.com>
2022-09-25 12:36:09 +0100

    query params and card nav

--- mind.lol/src/App.tsx
+++ mind.lol/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useLocation} from 'react-router-dom'
 import { chain, useNetwork, createClient, useContractRead, WagmiConfig } from 'wagmi'
 import { ConnectKitProvider, getDefaultClient } from 'connectkit'
 import queue from './artifacts/Queue.sol/Queue.json'
@@ -38,12 +39,12 @@ const Card = ({ gif, index, total }: any) => {
 )
 }
 
-const QueueStory = ({ contractConfig, onSuccess }: any) => {
+const QueueStory = ({ contractConfig, story, onSuccess }: any) => {
   // conditional hook as rendered component
   useContractRead({
     ...contractConfig,
     functionName: 'getPrompts',
-    args: [36],
+    args: [story || 36],
     onSuccess: async (data) => {
       // const metadata = await parseMetadata(data)
       console.log('getPrompts', data)
@@ -56,30 +57,45 @@ const QueueStory = ({ contractConfig, onSuccess }: any) => {
 
 
 function App() {
+  const search = useLocation().search
+  const chainId = new URLSearchParams(search).get('chain')
+  const story = new URLSearchParams(search).get('story')
+
   const [stories, setStories] = React.useState<string[]>([])
   const [fetchStories, setFetchStories] = React.useState<boolean>(true)
   const [contractConfig, setContractConfig] = React.useState<ConfigProviderState>({
-    addressOrName: supportedContracts.goerli && supportedContracts.mumbai,
+    addressOrName: (chainId && chainId === 'goerli') ? supportedContracts.goerli : supportedContracts.mumbai,
     contractInterface: queue.abi
   })
+
 
   const handleStory = (data: any[]) => {
     setStories(data)
     setFetchStories(false)
   }
 
+  const handleCardClick = (e: any, index: number) => {
+    console.log('click',  e)
+    e.preventDefault()
+    // console.log(stories.slice(index, stories.length).concat(stories.slice(0, index)))
+    setStories(stories.slice(e, stories.length).concat(stories.slice(0, index)))
+  }
+
   return (
     <WagmiConfig client={client}>
       <ConnectKitProvider>
         <div className="App">
-          {contractConfig.addressOrName && fetchStories && <QueueStory contractConfig={contractConfig} onSuccess={handleStory} />}
+          {contractConfig.addressOrName && fetchStories && (
+            <QueueStory contractConfig={contractConfig} story={story} onSuccess={handleStory} />
+          )}
           <header className="App-header">
             {stories.map((story, index) => (
+              <div key={index} onClick={e => handleCardClick(e, index)}>
               <Card
-                key={index}
                 gif={story[1]}
                 index={index}
                 total={stories.length}/>
+                </div>
             ))}
           </header>
         </div>

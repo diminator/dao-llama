395d4af01a092044f90970a5dc786c9b96595dee
diminator <dimi.dejonghe@gmail.com>
2022-09-25 14:49:35 +0100

    slider

--- mind.lol/src/App.tsx
+++ mind.lol/src/App.tsx
@@ -28,13 +28,13 @@ const Card = ({ gif, index, total }: any) => {
   const offsetY = 50 * ((index + 1) / total) ** 1
   const size = 600 * ((index + 1) / total) ** 4
   return (
-    <div className="Card-container">
-      <img key={gif} src={gif} className="Card-img" style={{
-        top: `calc(${offsetY}vh - ${size/2}px)`,
-        left: `calc(${offsetX}vw - ${size/2}px)`,
-        width: `${size}px`,
-        height: `${size}px`
-      }}/>
+    <div className="Card-container" style={{
+      top: `calc(${offsetY}vh - ${size/2}px)`,
+      left: `calc(${offsetX}vw - ${size/2}px)`,
+      width: `${size}px`,
+      height: `${size}px`
+    }}>
+      <img key={gif} src={gif} className="Card-img"/>
     </div>
 )
 }
@@ -44,11 +44,12 @@ const QueueStory = ({ contractConfig, story, onSuccess }: any) => {
   useContractRead({
     ...contractConfig,
     functionName: 'getPrompts',
-    args: [story || 36],
+    args: [story],
     onSuccess: async (data) => {
       // const metadata = await parseMetadata(data)
       console.log('getPrompts', data)
-      data.length && onSuccess(data as string[])
+      data.length && onSuccess(
+        data.map((story, index) => [index, ...story]) as any[])
     }
   })
 
@@ -58,32 +59,52 @@ const QueueStory = ({ contractConfig, story, onSuccess }: any) => {
 
 function App() {
   const search = useLocation().search
+  const paramStory = new URLSearchParams(search).get('story')
   const chainId = new URLSearchParams(search).get('chain')
-  const story = new URLSearchParams(search).get('story')
-
-  const [stories, setStories] = React.useState<string[]>([])
-  const [fetchStories, setFetchStories] = React.useState<boolean>(true)
-  const [contractConfig, setContractConfig] = React.useState<ConfigProviderState>({
+  const contractConfig : ConfigProviderState = {
     addressOrName: (chainId && chainId === 'goerli') ? supportedContracts.goerli : supportedContracts.mumbai,
     contractInterface: queue.abi
-  })
+  }
+
+  const [story, setStory] = React.useState<number>(
+    paramStory && parseInt(paramStory, 10) || 36)
+  const [stories, setStories] = React.useState<string[]>([])
+  const [active, setActive] = React.useState<number>(0)
+  const [fetchStories, setFetchStories] = React.useState<boolean>(true)
+  const [fetchedStories, setFetchedStories] = React.useState<number[]>([])
 
 
   const handleStory = (data: any[]) => {
-    setStories(data)
+    if (stories.length) {
+      const parsedStories = data.concat(stories).map((story, index) => {
+        console.log(index, stories.length, story[0])
+        if (index >= data.length)
+          story[0] += data.length
+        return story
+      })
+      setStories(parsedStories)
+      setActive(active + data.length - 1)
+    } else {
+      setStories(data)
+      setActive(data.length - 1)
+    }
+    fetchedStories.push(story)
+    setFetchedStories(fetchedStories)
     setFetchStories(false)
   }
 
-  const handleCardClick = (e: any, index: number) => {
-    console.log('click',  e)
-    e.preventDefault()
-    const shiftedStory = stories.slice(index, stories.length)
-      .concat(stories.slice(0, index))
-    console.log(shiftedStory)
-    // console.log(stories.slice(index, stories.length).concat(stories.slice(0, index)))
-    setStories(shiftedStory)
+  const handleCardClick = (e: any, storyIndex: number, activeIndex:number) => {
+    if ( activeIndex / stories.length < 0.5) {
+      setStory(story - 1)
+      setFetchStories(true)
+    }
+    setActive(storyIndex)
   }
 
+  const shiftedStories = active ? stories
+      .slice(active + 1, stories.length)
+      .concat(stories.slice(0, active + 1)) : stories
+  console.log(stories, shiftedStories, active)
   return (
     <WagmiConfig client={client}>
       <ConnectKitProvider>
@@ -92,13 +113,14 @@ function App() {
             <QueueStory contractConfig={contractConfig} story={story} onSuccess={handleStory} />
           )}
           <header className="App-header">
-            {stories.map((story, index) => (
-              <div key={index} onClick={e => handleCardClick(e, index)}>
-              <Card
-                gif={story[1]}
-                index={index}
-                total={stories.length}/>
-                </div>
+            {
+              shiftedStories.map((story: any, index: number) => (
+              <div key={story[2]} onClick={e => handleCardClick(e, story[0], index)}>
+                <Card
+                  gif={story[2]}
+                  index={index}
+                  total={stories.length}/>
+              </div>
             ))}
           </header>
         </div>
